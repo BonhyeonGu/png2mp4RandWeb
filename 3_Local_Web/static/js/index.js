28a3bd1a4ef27cda10f0b6abd97244c1f8e9bc67
whwhdtk <8whwhdtk8@naver.com>
2024-10-26 19:39:24 +0900

    사이즈 추가

--- 3_Local_Web/static/js/index.js
+++ 3_Local_Web/static/js/index.js
@@ -1,32 +1,34 @@
 let selectedPhotos = [];
 let currentIndex = 0;
+let totalImages = 0;
+
+function updateImage(index) {
+    const imgTag = document.getElementById('photo');
+    const newImageUrl = `/photo/${index}?${new Date().getTime()}`;
+    imgTag.setAttribute('src', newImageUrl);
+}
+
+function showNextImage() {
+    currentIndex = (currentIndex + 1) % totalImages;
+    updateImage(currentIndex);
+}
+
+function showPreviousImage() {
+    currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+    updateImage(currentIndex);
+}
 
 function fetchImageList() {
     fetch('/api/images')
         .then(response => response.json())
         .then(data => {
             selectedPhotos = data.images;
-            if (selectedPhotos.length > 0) {
+            totalImages = selectedPhotos.length;
+            if (totalImages > 0) {
                 updateImage(currentIndex);
             }
         })
         .catch(error => console.error('Error fetching image list:', error));
-}
-
-function updateImage(index) {
-    const imgTag = document.getElementById('photo');
-    const newImageUrl = `${selectedPhotos[index]}?${new Date().getTime()}`;
-    imgTag.setAttribute('src', newImageUrl);
-}
-
-function showNextImage() {
-    currentIndex = (currentIndex + 1) % selectedPhotos.length;
-    updateImage(currentIndex);
-}
-
-function showPreviousImage() {
-    currentIndex = (currentIndex - 1 + selectedPhotos.length) % selectedPhotos.length;
-    updateImage(currentIndex);
 }
 
 document.getElementById('changeButton').addEventListener('click', showNextImage);
